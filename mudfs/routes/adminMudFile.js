0a308677d0301c4637b7f565e229bc17cd8ed7dc
Angelo Feraudo <aferaudo34@gmail.com>
2019-10-28 13:25:29 +0000

    Add session expiration and management

--- mudfs/routes/adminMudFile.js
+++ mudfs/routes/adminMudFile.js
@@ -11,6 +11,8 @@ let upload = multer({dest: '/upload'});
 /* Initial page to upload a mud file*/
 router.get('/', function(req, res, next) {
     if(req.session.loggedin){
+        var hour = 3600000
+        req.session.cookie.expires = new Date(Date.now() + hour)
         console.log('request admin page')
         //var list = mudController.mudFileListNoRend(next)
         controller.initial(req,res, next);
@@ -21,17 +23,22 @@ router.get('/', function(req, res, next) {
 
 /* Post request for mudFile upload */
 router.post('/upload', upload.single('mudFileUpload'),function(req, res, next) {
-    if (!req.file) {
-        return next(new Error("No file received"));
+    if(req.session.loggedin){
+        if (!req.file) {
+            return next(new Error("No file received"));
+        }else
+            controller.mudFileInsert(req, res)
+            //res.send('File successfully updated')
     }else
-        controller.mudFileInsert(req, res)
-        //res.send('File successfully updated')
+        res.redirect('/login')
 });
 
 router.post('/remove', function(req, res, next) {
-
+    if(req.session.loggedin){
         console.log('File to delete ' + req.body.filename)
         controller.deleteByName(req,res)
+    }else
+        res.redirect('/login')
 });
 
 
--- mudfs/routes/login.js
+++ mudfs/routes/login.js
@@ -4,13 +4,20 @@ let controller = require('../controller/userController')
 
 
 router.get('/',function(req, res, next){
-    console.log("Requested login page")
-    res.render('login', {title: "Login Page"})
+    if(req.session.loggedin)
+        res.redirect('/admin'); // to change with the page of the user logged in!
+    else 
+    {
+        console.log("Requested login page")
+        res.render('login', {title: "Login Page"})
+    }
 })
 /* Post Request */
 router.post('/',function(req, res, next) {
     //console.log(req.body.username)
     //console.log(req.body.password)
+    
+    
     controller.authUser(req.body.username, req.body.password, function (err, user){
         if (err) {
           return next(err);
